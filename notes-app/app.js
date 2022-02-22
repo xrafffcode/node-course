dcf15a7bc03ebd34c0b1f026fe88af50822acba7
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-02-22 17:58:51 +0700

    Importing npm Modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,8 @@
-
+const validator = require('validator');
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
 
 console.log(msg);
+
+console.log(validator.isURL('http/google.com'));
---
+++ notes-app/utils.js
@@ -0,0 +1,9 @@
+console.log('utils.js');
+
+const name = 'Andrew';
+
+const add = function (a, b) {
+    return a + b;
+}
+
+module.exports = add;
