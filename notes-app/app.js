66892e42237e9c92934665bc3b4bd61563cadb4f
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-02-22 18:26:12 +0700

    Global npm Modules and nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,12 +4,5 @@ const getNotes = require('./notes.js');
 const msg = getNotes();
 console.log(msg);
 
-//
-// Challenge: Use the chalk library in your project
-//
-// 1. Install chalk 2.4.1
-// 2. Load chalk into app.js
-// 3. Use it to print out a message in green
-// 4. Test your work
 
-console.log(chalk.green.inverse('Success!'));
+console.log(chalk.red.inverse('Error!'));
