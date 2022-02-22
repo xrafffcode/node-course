0ad5f41db5467a441ed1a8fab3ebfa4658170456
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-02-22 18:15:55 +0700

    Printing in Color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,15 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
-
 console.log(msg);
 
-console.log(validator.isURL('http/google.com'));
+//
+// Challenge: Use the chalk library in your project
+//
+// 1. Install chalk 2.4.1
+// 2. Load chalk into app.js
+// 3. Use it to print out a message in green
+// 4. Test your work
+
+console.log(chalk.green.inverse('Success!'));
